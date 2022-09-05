8527dd4c75593c6aeb078b63d204c48fcc295e5e
laodu <laodu5244@163.com>
2022-09-05 23:12:42 +0800

    add

---
+++ uni_06 组件库/pages/card/card.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<uni-card title="基础卡片" extra="额外信息">
+			<text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
+		</uni-card>
+		
+		<uni-card title="基础卡片" sub-title="副标题" extra="额外信息" thumbnail="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/460d46d0-4fcc-11eb-8ff1-d5dcf8779628.png">
+			<text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
+		</uni-card>
+		
+		<uni-card title="基础卡片" :isFull="true" sub-title="副标题" extra="额外信息">
+			<text>这是一个通栏卡片 ，通栏没有外边距，左右会贴合父元素。</text>
+		</uni-card>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
---
+++ uni_06 组件库/pages/index/index.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		
+		<!--  引入日期插件 -->
+		<view>
+			<uni-calendar 
+			:insert="true"
+			:lunar="true" 
+			ref="calendar"
+			@change="change"/>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: 'Hello',
+		}
+	},
+	methods: {
+		open(){
+			this.$refs.calendar.open();
+		},
+		confirm(e) {
+			console.log(e);
+		},
+		change(e){
+			console.log('change')
+		},
+	}
+}
+</script>
+
+<style>
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+
+.logo {
+	height: 200rpx;
+	width: 200rpx;
+	margin-top: 200rpx;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 50rpx;
+}
+
+</style>
